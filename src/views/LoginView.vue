cbcbda514c5f9e364a0713ec3925e906bbdd7884
maisiang <maisiang001@gmail.com>
2022-12-08 00:37:21 +0800

    新增組件 Header、TabBar、Swiper 新增頁面 CartView、LoginView、ProductsView、SearchView、UsersView，添加到routes 新增SearchView子路由 Home.vue、List.vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">login</div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/search/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="search-home">home</div>
+  </template>
+  
+  <script>
+  export default {
+    name: "SearchHome",
+  };
+  </script>
+  
+  <style scoped></style>
+  
