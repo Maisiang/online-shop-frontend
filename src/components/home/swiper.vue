ad7603aa60ccec47c19ce1b0f6df211ede713f8b
maisiang <maisiang001@gmail.com>
2022-12-08 02:44:53 +0800

    修正組件 Swiper pagination

--- src/components/home/swiper.vue
+++ src/components/home/swiper.vue
@@ -1,18 +1,24 @@
 <template>
-    <div class="swiper bg">
-        <div class="swiper-wrapper">
-            <img class="swiper-slide page1"
-            v-for="(item,index) in swiperList" 
-            :key="index" :src="item.imgUrl">
-        </div>
-        <div class="swiper-pagination" slot="pagination"></div>
+    <div class="swiper">
+        <swiper :modules="modules"
+                :slides-per-view="1"      
+                :loop=true
+                :autoplay="{ delay: 2000}"
+                :speed="2000"
+                :pagination="{ clickable: true }">
+            <swiper-slide v-for="(item,index) in swiperList" :key="index">
+                <img :src="item.imgUrl">
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
-import Swiper , {Autoplay , Pagination} from 'swiper';
+import { Autoplay, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
+import 'swiper/css/pagination';
+
 export default {
     data(){
         return{
@@ -23,26 +29,14 @@ export default {
             ],
         }
     },
-    setup(){
-        // 使用 Swiper 需要等它的DOM都渲染到網頁上後
-        // 才可以取得到它 div.swiper 這個DOM
-        // 所以要在 onMounted 的地方 new Swiper。
-        onMounted(()=> {
-            const swiper = new Swiper(".swiper", {
-                modules: [Autoplay , Pagination],
-                loop:true,
-                autoplay: {
-                    delay: 2000,
-                },
-                speed: 2000,
-                pagination:{// bug 版本問題?
-                    el:'.swiper-pagination',
-                    clickable: true,
-                },
-                
-            });
-
-        });
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Autoplay, Pagination],
+      };
     },
 }
 </script>
@@ -51,5 +45,8 @@ export default {
 .swiper {
     width: 100%;
     height: 100%;
+}
+.Pagination{
+    margin-bottom: -53px;
 }
 </style>
