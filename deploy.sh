e76988d26301692f2cf1dc877e8176f56f33d4d6
maisiang <maisiang001@gmail.com>
2023-02-06 19:42:45 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,15 @@ set -e
 # 建立輸出檔案(打包編譯)
 npm run build
 
-# dist目錄初始化git
+# dist目錄初始化git並commit
 cd dist
+cp index.html 404.html
 git init 
 git add -A
 git commit -m 'deploy'
 
 
 # dist資料夾強制推送至遠端 gh-pages分支
-git push -f https://github.com/Maisiang/online-shop-frontend.git main:gh-pages
+git push -f https://github.com/Maisiang/online-shop-frontend.git master:gh-pages
 
 cd -
